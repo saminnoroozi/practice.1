e6e2587fe60dac7f47a6ff2b3d3352db59aee88f
saminnoroozi <153554191+saminnoroozi@users.noreply.github.com>
2023-12-12 15:58:01 +0330

    Update practice.3.py

--- practice.3.py
+++ practice.3.py
@@ -1,10 +1,10 @@
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 c=a*b
+print(c)
 d=a/b
+print(d)
 e=a+b
+print(e)
 f=a-b
-print(c,
-      d,
-      e,
-      f,)
+print(f)
